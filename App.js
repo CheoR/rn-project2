a7d88a0f0f7c10604f1599f35b67ad4655a2ef8f
CheoR <cheo.roman+github@gmail.com>
2022-07-17 10:26:32 -0500

    css: separate with styling

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+const DATA = [
+  { id: 1, text: "Text1" },
+  { id: 2, text: "Text2" },
+  { id: 3, text: "Text3" },
+  { id: 4, text: "Text4" },
+  { id: 5, text: "Text5" },
+  { id: 6, text: "Text6" },
+  { id: 7, text: "Text7" },
+  { id: 8, text: "Text8" },
+  { id: 9, text: "Text9" },
+  { id: 10, text: "Text10" },
+  { id: 11, text: "Text11" },
+  { id: 12, text: "Text12" },
+  { id: 13, text: "Text13" },
+  { id: 14, text: "Text14" },
+  { id: 15, text: "Text15" },
+  { id: 16, text: "Text16" },
+  { id: 17, text: "Text17" },
+  { id: 18, text: "Text18" },
+  { id: 19, text: "Text19" },
+  { id: 20, text: "Text20" },
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={DATA}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItems}
+        style={styles.flatList}
+      />
     </View>
   );
 }
@@ -13,8 +40,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+    backgroundColor: "orange",
+    // width: "100%",
+  },
+  flatList: {
+    backgroundColor: "green",
+    width: "100%",
+    borderColor: "red",
+    borderWidth: 8,
+    padding: 0,
+    margin: 0,
+  },
+  li: {
+    cntr: {
+      backgroundColor: "blue",
+      width: "100%",
+      alignItems: "center",
+      borderColor: "yellow",
+      borderWidth: 8,
+      margin: 8,
+    },
+    text: {
+      color: "yellow",
+    },
   },
 });
+
+const renderItems = ({ item }) => (
+  <View style={styles.li.cntr}>
+    <Text style={styles.li.text}>{item.text}</Text>
+  </View>
+);
