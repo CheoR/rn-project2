bf33f653b4ae17fa2ff1a827349452ccb883f47e
CheoR <cheo.roman+github@gmail.com>
2022-07-20 20:23:13 -0500

    ref: data into own dir

--- App.js
+++ App.js
@@ -16,28 +16,7 @@ import {
 } from "react-native";
 import styles from "./styles";
 
-const DATA = [
-  { id: 1, text: "Text1" },
-  { id: 2, text: "Text2" },
-  { id: 3, text: "Text3" },
-  { id: 4, text: "Text4" },
-  { id: 5, text: "Text5" },
-  { id: 6, text: "Text6" },
-  { id: 7, text: "Text7" },
-  { id: 8, text: "Text8" },
-  { id: 9, text: "Text9" },
-  { id: 10, text: "Text10" },
-  { id: 11, text: "Text11" },
-  { id: 12, text: "Text12" },
-  { id: 13, text: "Text13" },
-  { id: 14, text: "Text14" },
-  { id: 15, text: "Text15" },
-  { id: 16, text: "Text16" },
-  { id: 17, text: "Text17" },
-  { id: 18, text: "Text18" },
-  { id: 19, text: "Text19" },
-  { id: 20, text: "Text20" },
-];
+import DATA from "./data/data";
 
 const pressHandler = (text) => {
   console.log(text);
